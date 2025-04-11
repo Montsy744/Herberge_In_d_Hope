d70305da1d553d31268fc5a3ed65e29d2ae759f2
Montsy744 <straseelehugo@gmail.com>
2025-04-11 18:53:03 +0200

    ajout de nos services et mot du chef

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ const header = document.querySelector("header");
 const accueil = document.querySelector("#acceuil");
 
 // Créez un observateur pour suivre les entrées et sorties de la div #acceuil
-const observer = new IntersectionObserver((entries) => {
+const observer1 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       // Lorsque #acceuil est visible dans l'écran
@@ -16,4 +16,45 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 // Démarrez l'observation de la div #acceuil
-observer.observe(accueil);
+observer1.observe(accueil);
+
+// Sélectionner tous les éléments qui ont la classe 'reveal'
+const elements = document.querySelectorAll(".reveal");
+
+// Options pour l'Intersection Observer
+const options = {
+  root: null, // Utiliser la fenêtre de visualisation par défaut
+  threshold: 0.3, // Un élément est considéré comme visible si 30% de celui-ci est dans la vue
+};
+
+// Fonction de callback qui sera exécutée lors de l'intersection
+const handleIntersection = (entries, observer1) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Ajouter la classe 'visible' lorsque l'élément entre dans la vue
+      entry.target.classList.add("visible");
+      entry.target.classList.remove("disappear-up", "disappear-down"); // Enlever les classes de disparition
+    } else {
+      // Vérifier la direction du défilement
+      if (entry.boundingClientRect.top < 0) {
+        // L'élément sort par le haut
+        entry.target.classList.remove("visible");
+        entry.target.classList.add("disappear-up");
+      } else {
+        // L'élément sort par le bas
+        entry.target.classList.remove("visible");
+        entry.target.classList.add("disappear-down");
+      }
+    }
+  });
+};
+
+// Créer une instance d'IntersectionObserver avec la fonction de callback et les options
+const observer2 = new IntersectionObserver(handleIntersection, options);
+
+// Observer chaque élément avec la classe 'reveal'
+elements.forEach((element) => {
+  const delay = element.getAttribute("data-delay");
+  element.style.setProperty("--delay", `${delay}s`);
+  observer2.observe(element);
+});
