c96ef76a275e383791f992c1360a98a8a1bf4bca
Hugo Straseele <straseelehugo@gmail.com>
2026-02-16 20:22:48 +0100

    update des constant

--- constant/constant.js
+++ constant/constant.js
@@ -28,10 +28,31 @@ const textAmnbianceConv = "Profitez d’un environnement chaleureux et accueilla
 
 const textChef = "À l'Herberge In d'Hope, chaque plat qui sort de notre cuisine est un hommage à notre belle région et à ses saveurs d’antan. Ici, pas de chichis : juste des produits frais, des recettes qui sentent bon la tradition, et un chef qui cuisine avec le cœur. Ce que j’aime, c’est voir les sourires autour des assiettes, entendre les discussions animées, sentir les parfums qui rappellent les repas en famille. C’est ça, l’esprit de notre auberge : un lieu simple, vrai, où l’on prend le temps de bien manger et de bien vivre. Merci de faire partie de cette aventure culinaire. Prenez place, détendez-vous, et surtout… bon appétit !"
 
+const service1 = [
+    "Service traiteur",
+    "Séminaires d'entreprises",
+    "Réceptions de mariage",
+    "Trois salles de réception modulable, possibilité d'accueillir entre 15 et 250 personnes",
+    "Accessible aux personnes à mobilité réduite",
+]
+
+const service2 = [
+    "Coin enfants, jouets extérieurs à disposition",
+    "Terrasse",
+    "Parking en libre-service",
+    "Coin fumeurs",
+    "Wi-Fi gratuit",
+]
+
+const service3 = "Événements privés: Baptême, anniversaire, communion"
+
 export {
     liensNavbar,
     liensImg1,
     textApropos,
     textAmnbianceConv,
     textChef,
+    service1,
+    service2,
+    service3,
 };
